5c293bf3a8e6de9d8d30ea57222b3ba0c9cbf646
Alok-Dumet <aenugualok@gmail.com>
2026-09-01 11:43:15 -0400

    PRODUCT: Added index endpoint

--- backend/src/main/java/com/bilo/backend/controller/ProductController.java
+++ backend/src/main/java/com/bilo/backend/controller/ProductController.java
@@ -1,9 +1,11 @@
 package com.bilo.backend.controller;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,9 +27,16 @@ public class ProductController {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    //Might have to implement GraphQL or query parameters to stop additional fetches for categories
     @GetMapping("/products")
     public List<Product> getAllProducts() {
         return productRepository.findAll();
+    }
+
+    @GetMapping("/products/{id}")
+    public Product getProductById(@PathVariable UUID id) {
+        return productRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("Product not found"));
     }
 
     @PostMapping("/products")
