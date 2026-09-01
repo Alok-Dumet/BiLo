bf119c68a9157ffada34c358674ad12726ea19ff
Alok-Dumet <aenugualok@gmail.com>
2026-09-01 11:42:59 -0400

    CATEGORY: Added index endpoint

--- backend/src/main/java/com/bilo/backend/controller/CategoryController.java
+++ backend/src/main/java/com/bilo/backend/controller/CategoryController.java
@@ -1,16 +1,18 @@
 package com.bilo.backend.controller;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bilo.backend.dto.CreateCategoryRequest;
 import com.bilo.backend.model.Category;
 import com.bilo.backend.repository.CategoryRepository;
-import com.bilo.backend.dto.CreateCategoryRequest;
 
 import jakarta.validation.Valid;
 
@@ -23,6 +25,12 @@ public class CategoryController {
     @GetMapping("/categories")
     public List<Category> getAllCategories(){
         return categoryRepository.findAll();
+    }
+
+    @GetMapping("/categories/{id}")
+    public Category getCategoryById(@PathVariable UUID id) {
+        return categoryRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("Category not found"));
     }
 
     @PostMapping("/categories")
